d43466fdfe41844059d2707e7b68331e75b13f5f
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-16 21:11:03 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors')
 
-const PORT = 9090
+const PORT = 9090;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+app.use(cors())
 
-app.listen(PORT, () => console.log(`server is running at ${PORT}...`))
+const startMongodb = async() => {await mongoose
+  .connect(
+    "mongodb+srv://mern-stack:5o4QRFVzCIuGbX6L@cluster0.yza8k0o.mongodb.net/?retryWrites=true&w=majority"
+)}
+console.log('server is connected to mongodb database...');
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => console.log(`server is running at ${PORT}...`));
 
 // 5o4QRFVzCIuGbX6L    mern-stack
 
