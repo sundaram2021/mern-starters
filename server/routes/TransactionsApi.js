51a52bed6f987d3980b38168c137ab4824f4c94a
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-17 23:25:22 +0530

    added delete and patch routes

--- server/routes/TransactionsApi.js
+++ server/routes/TransactionsApi.js
@@ -20,4 +20,16 @@ router.post("/", async (req, res) => {
   res.json({ message: "transaction complete" });
 });
 
+router.delete("/:id", async (req, res) => {
+  // console.log(req.params.id);
+  await Transaction.findOneAndDelete({ _id: req.params.id });
+  // await Transaction.deleteOne({_id: req.params.id})
+  res.json({ message: "Transaction Deleted" });
+});
+
+router.patch("/:id", async (req, res) => [
+  await Transaction.updateOne({ _id: req.params.id }, { $set: req.body }),
+  res.json({ message: "Updated Successfully" })
+]);
+
 export default router;
