caa64e6df89e90d95acbbf22151229c3bb0bd62b
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-20 23:08:25 +0530

    fetching user data from token in the cooi=kies

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,43 @@
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import "./index.css";
 import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "./store/auth";
+import Cookies from "js-cookie";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  console.log(auth);
+
+  const fetchUser = async () => {
+    setIsLoading(true);
+    const token = Cookies.get("token");
+
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/user`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    // setIsAuthenticated(res.ok);
+
+    if (res.ok) {
+      const user = await res.json();
+      console.log(user);
+      dispatch(getUser(user));
+    }
+  };
+
+  useEffect(() => {
+    fetchUser()
+  }, []);
+
+  if(isLoading){
+    return <p>Loading....</p>
+  }
+
   return (
     <>
       <Navbar />
