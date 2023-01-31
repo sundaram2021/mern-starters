b8c7c7ff6d245d5988125ac8fd0402adf8f130c9
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-31 21:50:18 +0530

    modified

--- server/controllers/HomeController.js
+++ server/controllers/HomeController.js
@@ -1,21 +1,19 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 dotenv.config();
-import User from '../models/User.js';
+import User from "../models/User.js";
 
-export const Home  = async(req, res) => {
-    const token = req.headers['x-access-token'];
-    console.log(token);
-
-    try{
-       const match = jwt.verify(token, process.env.JWT_SECRET);
-       const email = match.email;
-       const user = await User.findOne({email});
-
-       return res.json({ status: "ok", email: user.email })
-    }catch(err){
-        // res.redirect('/login');
-        return res.status(406).json({ message: "Invalid Token" })
-    }
+export const Home = async (req, res) => {
+    const token = req.headers["x-access-token"];
+  try {
     
-}
+    const match = jwt.verify(token, process.env.JWT_SECRET);
+    const email = match.email;
+    const user = await User.findOne({ email });
+
+    return res.json({ status: "ok", email: user.email });
+  } catch (err) {
+    // req.session.message = "Please log in to access this page";
+    return res.status(406).json({message: "Internal server error"});
+  }
+};
