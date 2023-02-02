7f94c97097c908c1cdc14f2c92bb03b6ede1bc80
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-02 21:27:40 +0530

    added getdata route to fetch data from mongodb

--- server/routes/Home.js
+++ server/routes/Home.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import {Home, postTodo} from '../controllers/HomeController.js'
+import {Home, postTodo, getData} from '../controllers/HomeController.js'
 
 const router2 = Router();
 
 router2.get("/", Home);
 router2.post('/', postTodo);
+router2.get('/getdata', getData);
 
 export default router2;
