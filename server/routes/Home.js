2cff416b76049b2cabb8e9c59d7f733db6067311
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-05 20:34:31 +0530

    added delete and patch route

--- server/routes/Home.js
+++ server/routes/Home.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import {Home, postTodo, getData} from '../controllers/HomeController.js'
+import {Home, postTodo, getData, deleteTodo, editTodo} from '../controllers/HomeController.js'
 
 const router2 = Router();
 
 router2.get("/", Home);
 router2.post('/', postTodo);
 router2.get('/getdata', getData);
+router2.delete("/:id", deleteTodo)
+router2.patch("/:id", editTodo)
 
 export default router2;
