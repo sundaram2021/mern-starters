aa0a06eb21978bf93ac3c68b480930615e5d2bef
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-05 22:11:36 +0530

    modified

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from "react";
-import "../App.css"
-import { v4 as uuidv4 } from 'uuid';
+import "../App.css";
+import { v4 as uuidv4 } from "uuid";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import DeleteIcon from '@mui/icons-material/Delete';
-import { BiEdit } from 'react-icons/bi';
+import DeleteIcon from "@mui/icons-material/Delete";
+import { BiEdit } from "react-icons/bi";
 // import List from "@mui/material/List";
 // import ListItem from "@mui/material/ListItem";
 // import ListItemText from "@mui/material/ListItemText";
 
 function Home() {
-  const [submitBtn, setSubmitBtn] = useState(true)
-  const [refresh,  setRefresh] = useState(false)
+  const [todoId, setTodoId] = useState();
+  const [submitBtn, setSubmitBtn] = useState(true);
+  const [refresh, setRefresh] = useState(false);
   const [todos, setTodos] = useState({
     todo: "",
   });
@@ -22,7 +23,7 @@ function Home() {
 
   const token = localStorage.getItem("token");
 
-  window.addEventListener("storage", function (event) {
+  window.addEventListener("storage", function(event) {
     if (event.key === "token" && !event.newValue) {
       window.location.replace("/login");
     }
@@ -49,9 +50,24 @@ function Home() {
   // console.log(todos.todo);
 
   const submitTodo = async () => {
+    if (!submitBtn) {
+      console.log(todos.todo);
+      // const { todo } = todos;
+      const res = await fetch(`http://localhost:9090/${todoId}`, {
+        method: "PUT",
+        body: JSON.stringify(todos),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
 
-    if(!submitBtn) return;
-    
+      if(res.ok){
+        console.log('Todo edited')
+        window.location.reload(true);
+      }
+      return;
+    }
+
     const res = await fetch("http://localhost:9090", {
       method: "POST",
       body: JSON.stringify(todos),
@@ -65,10 +81,10 @@ function Home() {
       // console.log("all ok");
       const { savedTodo } = await res.json();
       // setTasks([...tasks, savedTodo]);
-      
-      console.log('savedTodo => '+ savedTodo);
+
+      console.log("savedTodo => " + savedTodo);
     }
-    setRefresh(true)
+    setRefresh(true);
   };
 
   useEffect(() => {
@@ -76,8 +92,8 @@ function Home() {
     getMongodbData();
   }, []);
 
-  async function getMongodbData(){
-    const res = await fetch('http://localhost:9090/getdata', {
+  async function getMongodbData() {
+    const res = await fetch("http://localhost:9090/getdata", {
       headers: {
         "x-access-token": token,
         Accept: "application/json",
@@ -91,51 +107,39 @@ function Home() {
     // p = myTodos;
     setTasks(myTodos);
     setTodos({
-      todo: ""
-    })
+      todo: "",
+    });
   }
 
-  if(refresh){
-    window.location.reload(true)
+  if (refresh) {
+    window.location.reload(true);
   }
 
-  const editHandler = async(id) => {
+  const editHandler = async (id) => {
     setSubmitBtn(false);
-    const getTodo = tasks.filter((task) => task._id === id)
+    setTodoId(id)
+    const getTodo = tasks.filter((task) => task._id === id);
     const myTodo = getTodo[0].todo;
     // console.log(myTodo, getTodo)
 
-    setTodos({...todos, todo: myTodo})
-    console.log(todos.todo)
+    setTodos({ ...todos, todo: myTodo });
+    // console.log(todos.todo)
+  };
 
-    // const res = await fetch(`http://localhost:9090/:${id}`, {
-    //   body: JSON.stringify(myTodo),
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   }
-    // })
-
-    // if(res.ok){
-    //   console.log('Todo edited')
-    //   window.location.reload(true);
-    // }
-  }
-
-  const deleteHandler = async(id) => {
-    console.log(id)
+  const deleteHandler = async (id) => {
+    console.log(id);
     const res = await fetch(`http://localhost:9090/${id}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
-      }
+      },
+    });
 
-    })
-
-    if(res.ok){
-      console.log('todo deleted')
+    if (res.ok) {
+      console.log("todo deleted");
       window.location.reload(true);
     }
-  }
+  };
 
   return (
     <>
@@ -166,14 +170,19 @@ function Home() {
           />
           <Stack spacing={2} direction="row" style={{ height: "54px" }}>
             <Button variant="contained" onClick={submitTodo}>
-             {submitBtn ?  'Submit' : 'Modify'}
+              {submitBtn ? "Submit" : "Modify"}
             </Button>
           </Stack>
         </div>
       </Box>
       <ul className="ul">
         {tasks.map((item) => (
-          <li key={uuidv4()}>{item.todo} &nbsp;&nbsp;&nbsp;<DeleteIcon onClick={() => deleteHandler(item._id)} />&nbsp;&nbsp;&nbsp;<BiEdit className="size" onClick={() => editHandler(item._id)}/></li>
+          <li key={uuidv4()}>
+            {item.todo} &nbsp;&nbsp;&nbsp;
+            <DeleteIcon onClick={() => deleteHandler(item._id)} />
+            &nbsp;&nbsp;&nbsp;
+            <BiEdit className="size" onClick={() => editHandler(item._id)} />
+          </li>
         ))}
       </ul>
     </>
